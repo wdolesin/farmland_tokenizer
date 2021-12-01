1f37276e23e82dfdedf8a086760bf6cf68707fbb
sangramsinghg <sangramsinghg@yahoo.com>
2021-11-30 18:22:10 -0800

    added streamlit support for the crowdsale

--- farmland_token_app.py
+++ farmland_token_app.py
@@ -4,11 +4,21 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+import SessionState
+from selenium import webdriver
 
 load_dotenv()
 
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+#w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+
+# Define the EXTENSION PATH
+# EXTENSION_PATH = '.'
+# opt = webdriver.ChromeOptions()
+# opt.add_extension(EXTENSION_PATH)
+
+# driver = webdriver.Chrome(chrome_options=opt)
 
 ################################################################################
 # Contract Helper function:
@@ -42,9 +52,10 @@ crowdsale_contract = load_crowdsale_contract("./contracts/compiled/farmland_toke
 token_contract = load_crowdsale_contract("./contracts/compiled/farmland_token_abi.json",
                                             "SMART_CONTRACT_TOKEN_ADDRESS")
 
+session_state = SessionState.get(name="", button_purchase=False)
 st.markdown("# The Farmland Crowdsale")
 
-farmland_options = ["Crowdsale status", "Purchase tokens"]
+farmland_options = ["Crowdsale status", "Purchase tokens", "Set Rate"]
 
 # farmland_options_df = pd.DataFrame(farmland_options)
 # st.write(pychain_df)
@@ -52,7 +63,7 @@ farmland_options = ["Crowdsale status", "Purchase tokens"]
 # difficulty = st.sidebar.slider("Block Difficulty", 1, 5, 2)
 # pychain.difficulty = difficulty
 
-st.sidebar.write("# Option Selected")
+st.sidebar.write("# Options")
 selected_option = st.sidebar.selectbox(
     "Which option would you like to choose?", farmland_options
 )
@@ -78,7 +89,7 @@ if selected_option == "Crowdsale status":
     weiRaised = crowdsale_contract.functions.weiRaised().call()
     st.write(f"## Wei Raised               : {weiRaised}")
     openingTime = crowdsale_contract.functions.openingTime().call()
-    st.write(f"## Opening Time             : {openingTime}")
+    st.write(f"## Opening Time             : {openingTime}") # TODO change the formatting
     closingTime = crowdsale_contract.functions.closingTime().call()
     st.write(f"## Closing Time             : {closingTime}")
     currentTime = crowdsale_contract.functions.currentTime().call()
@@ -89,30 +100,44 @@ else:
 ################################################################################
     accounts = w3.eth.accounts
     purchasers_account = accounts[0]
-    purchasers_account = st.selectbox("Select Account", options=accounts)
-    crowdsale_contract_address = os.getenv("SMART_CONTRACT_CROWDSALE_ADDRESS")
-    tokens_desired = int(st.number_input("Enter tokens desired"))
-    wallet = crowdsale_contract.functions.wallet().call()
-    st.write(f"### Wallet is {wallet}")
-    st.write(f"### Wallet's balance is {w3.eth.getBalance(wallet)} weis")
     symbol = token_contract.functions.symbol().call()
     cap = crowdsale_contract.functions.cap().call()
     st.write(f"### Total {symbol} Tokens             : {cap}")
     total_token_available = token_contract.functions.totalSupply().call()
     st.write(f"### Total {symbol} Tokens Issued      : {total_token_available}")
+    st.write("")
+
+    # if (w3.eth.accounts)
+    # purchasers_account = w3.eth.getAccounts()
+    #
+    accounts.append("Enter Manually")
+    purchasers_account = st.selectbox("Select Account", options=accounts)
+    manual_purchasers_account = st.text_input("Enter Account")
+    crowdsale_contract_address = os.getenv("SMART_CONTRACT_CROWDSALE_ADDRESS")
+    tokens_desired = int(st.number_input("Enter tokens desired")) # TODO , format = '{:,d}'
+    wallet = crowdsale_contract.functions.wallet().call()
+    st.write(f"#### Farmer's Wallet           : {wallet}")
+    st.write(f"#### Wallet's balance : {w3.fromWei(w3.eth.getBalance(wallet), 'ether')} Eth")
+
+    if purchasers_account == "Enter Manually":
+        purchasers_account = manual_purchasers_account
     balance = token_contract.functions.balanceOf(purchasers_account).call()
-    st.write(f"### This account currently holds {balance} {symbol} tokens")
-    st.write(f"### account balance is {w3.eth.getBalance(purchasers_account)} weis")
-    if st.button("Get Wei Estimate"):
+    if total_token_available:
+        st.write(f"#### Account holds    : {balance} {symbol} tokens ({(balance/total_token_available):.02f}%)")
+    else:
+        st.write(f"#### Account holds    : {balance} {symbol} tokens ({(balance):.02f}%)")
+
+    st.write(f"#### Account balance  : {w3.fromWei(w3.eth.getBalance(purchasers_account), 'ether')} Eth")
+    st.write("")
+    if tokens_desired > 0:
         rate = crowdsale_contract.functions.rate().call()
-        st.write(f"### rate : {rate}")
+        st.write(f"#### rate : {rate}")
         wei_estimate = crowdsale_contract.functions.getWeiEstimate(tokens_desired).call()
-        st.write(f"### Wei Estimate for {tokens_desired} tokens : {wei_estimate}")    
-    if st.button("Purchase"):
-        st.write(f"Purchasing {tokens_desired} tokens ....")
-        wei_estimate = crowdsale_contract.functions.getWeiEstimate(tokens_desired).call()
-        st.write(f"### Wei Estimate for {tokens_desired} tokens : {wei_estimate}")
-        
+        st.write(f"#### Wei Estimate for {tokens_desired} tokens : {wei_estimate}")
+    if st.button("Purchase") and tokens_desired > 0:
+        st.write(f"### Purchasing {tokens_desired} tokens ....")
+        # wei_estimate = crowdsale_contract.functions.getWeiEstimate(tokens_desired).call()
+        # st.write(f"### Wei Estimate for {tokens_desired} tokens : {wei_estimate}")
         tx_hash = crowdsale_contract.functions.buyTokens(purchasers_account).transact({
             "from":purchasers_account,
             "gas":1000000,
@@ -122,10 +147,9 @@ else:
         })
         w3.eth.wait_for_transaction_receipt(tx_hash)
         st.write(f"### Done purchasing {tokens_desired} tokens")
-        st.write(f"### Wallet balance now is {w3.eth.getBalance(wallet)} weis")
-        st.write(f"### account balance now is {w3.eth.getBalance(purchasers_account)} weis")
-
-
+        st.write(f"### Wallet balance now is {w3.fromWei(w3.eth.getBalance(wallet), 'ether')} Eth")
+        st.write(f"### account balance now is {w3.fromWei(w3.eth.getBalance(purchasers_account), 'ether')} Eth")
+        
 ################################################################################
 # Award Certificate
 ################################################################################
