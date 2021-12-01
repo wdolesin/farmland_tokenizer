f2592f5b95fb5eaac69b00a2eee4611764d807a5
sangramsinghg <sangramsinghg@yahoo.com>
2021-12-01 14:27:03 -0800

    Added comments to the javascript file and formatting in the create crowdsale html

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,104 +1,16 @@
-//initializing Moralis
-// Moralis.initialize("q6Mh6vo1F36tDuhfGqDw5DfOGXyeMK649eMFnpj2");
-// Moralis.serverURL = "https://hadft4aqgts5.usemoralis.com:2053/server";
 
-// Copyright 2017 https://tokenmarket.net - MIT licensed
-//
-// Run with Node 7.x as:
-//
-// node --harmony-async-await  deploy.js
-//
 
-/*
-let fs = require("fs");
-let Web3 = require('web3'); // https://www.npmjs.com/package/web3
-*/
-
-// Create a web3 connection to a running geth node over JSON-RPC running at
-// http://localhost:8545
-// For geth VPS server + SSH tunneling see
-// https://gist.github.com/miohtama/ce612b35415e74268ff243af645048f4
-// let web3 = new Web3(window.ethereum);
-//web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-/* Creating a contract object */
+// Check if metamask is loaded or not
 if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
 } else {
     console.log('MetaMask is not installed! PLEASE Install it from https://metamask.io/');
 }
+// instantiate the metamask
 window.web3 = new Web3(window.ethereum);
 
-/*
-if (provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavior.
-    await web3Modal.clearCachedProvider();
-    provider = null;
- }
- */
-
-// Read the compiled contract code
-// Compile with
-// solc SampleContract.sol --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc > contracts.json
-// let source = fs.readFileSync("contracts.json");
-// let contracts = JSON.parse(source)["contracts"];
-
-// ABI description as JSON structure
-// let abi = JSON.parse(contracts.SampleContract.abi);
-
-// Smart contract EVM bytecode as hex
-// let code = '0x' + contracts.SampleContract.bin;
-
-// Create Contract proxy class
-// let SampleContract = web3.eth.contract(abi);
-
-// Unlock the coinbase account to make transactions out of it
-/*console.log("Unlocking coinbase account");
-var password = "";
-try {
-  web3.personal.unlockAccount(web3.eth.coinbase, password);
-} catch(e) {
-  console.log(e);
-  return;
-}
-
-
-console.log("Deploying the contract");
-let contract = SampleContract.new({from: web3.eth.coinbase, gas: 1000000, data: code});
-*/
-// Transaction has entered to geth memory pool
-// console.log("Your contract is being deployed in transaction at http://testnet.etherscan.io/tx/" + contract.transactionHash);
-
-// http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-/*
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-*/
-// We need to wait until any miner has included the transaction
-// in a block to get the address of the contract
-/*
-async function waitBlock() {
-  while (true) {
-    let receipt = web3.eth.getTransactionReceipt(contract.transactionHash);
-    if (receipt && receipt.contractAddress) {
-      console.log("Your contract has been deployed at http://testnet.etherscan.io/address/" + receipt.contractAddress);
-      console.log("Note that it might take 30 - 90 sceonds for the block to propagate befor it's visible in etherscan.io");
-      break;
-    }
-    console.log("Waiting a mined block to include your contract... currently in block " + web3.eth.blockNumber);
-    await sleep(4000);
-  }
-}
-
-waitBlock();
-*/
-
 var address = 0;
+// Farmland Token Abi
 const FarmlandTokenAbi = [
 	{
 		"constant": false,
@@ -573,6 +485,7 @@ const FarmlandTokenAbi = [
 	}
 ]
 
+// Farmland Crowdsale Abi
 const FarmlandCrowdsaleTokenAbi = [
 	{
 		"constant": false,
@@ -955,6 +868,7 @@ const FarmlandCrowdsaleTokenAbi = [
 	}
 ]
 
+// Farmland Crowdsale Deployer ABI
 const FarmlandCrowdsaleDeployerAbi = [
 	{
 		"inputs": [
@@ -1040,39 +954,37 @@ const FarmlandCrowdsaleDeployerAbi = [
 	}
 ] 
 
-var FarmlandToken = 0; // new window.web3.eth.Contract(FarmlandTokenAbi, "0x04f48A187821BbE5fC72A292B60aB7C8Dd251dC2");
-var FarmlandCrowdsaleToken = 0; // new window.web3.eth.Contract(FarmlandCrowdsaleTokenAbi, "0x996E266C08F68A8CFD73Ee110b1a3CAF91c3763E");
+var FarmlandToken = 0; 
+// The default hard coded contract instantiation for future reference - 
+// new window.web3.eth.Contract(FarmlandTokenAbi, "0x04f48A187821BbE5fC72A292B60aB7C8Dd251dC2");
+var FarmlandCrowdsaleToken = 0; 
+// The default hard coded contract instantiation for future reference - 
+// new window.web3.eth.Contract(FarmlandCrowdsaleTokenAbi, "0x996E266C08F68A8CFD73Ee110b1a3CAF91c3763E");
 
+// Check if the page is loaded
 window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
 });
 
+// Enabled deployer Address upon load of the page
 window.onload = (event) => {
     console.log('The page has fully loaded - onload');
-    // var farmlandTokenAddress = localStorage.getItem('farmlandTokenAddress');
-    // var farmlandCrowdsaleAddress = localStorage.getItem('farmlandCrowdsaleAddress');
-    // console.log(farmlandTokenAddress);
-    // console.log(farmlandCrowdsaleAddress);
-    // FarmlandToken = new window.web3.eth.Contract(FarmlandTokenAbi, farmlandTokenAddress);
-    // FarmlandCrowdsaleToken = new window.web3.eth.Contract(FarmlandCrowdsaleTokenAbi, farmlandCrowdsaleAddress);
     document.getElementById("deployerAddress").removeAttribute("disabled");
-    // parcel_details();
-
 };
 
 //dApp frontend logic
 
+// Get current account of the purchaser from metamask and update the currentAddress text box
 async function get_current_account() {
     var account = await window.web3.eth.getAccounts();
     var userAccount = account[0];
 
-    // console.log(account[0]);
-    // get_account();
     document.getElementById("currentAccount").setAttribute("value", userAccount);
     window.web3.eth.defaultAccount = userAccount;
     return userAccount;
 }
 
+// Get the parcel details when the appropriate button is invoked
 async function parcel_details() {
     console.log("Parcel Called")
     var deployerAddress = document.getElementById("deployerAddress").value;
@@ -1099,27 +1011,43 @@ async function parcel_details() {
     parcel_tokens_issued();
     parcel_wei_raised();
 }
+
+// Get parcel owner from the contract
 async function parcel_owner() {
     FarmlandToken.methods.parcelOwner().call().then(parcel_owner_populate);
 }
+
+// Get the parcel acres from the contract
 async function parcel_acres() {
     FarmlandToken.methods.totalAcres().call().then(parcel_acres_populate);
 }
+
+// Get the parcel contract name 
 async function parcel_name() {
     FarmlandToken.methods.name().call().then(parcel_name_populate);
 }
+
+// Get the parcel contract symbol
 async function parcel_symbol() {
     FarmlandToken.methods.symbol().call().then(parcel_symbol_populate);
 }
+
+// Get the maximum parcel contract tokens
 async function parcel_cap() {
     FarmlandCrowdsaleToken.methods.cap().call().then(parcel_cap_populate);
 }
+
+// Get the parcel contract tokens issued
 async function parcel_tokens_issued() {
     FarmlandToken.methods.totalSupply().call().then(parcel_tokens_issued_populate);
 }
+
+// Get the parcel wei raised
 async function parcel_wei_raised() {
     FarmlandCrowdsaleToken.methods.weiRaised().call().then(parcel_wei_raised_populate);
 }
+
+// Get the account from the metamask
 async function get_account() {
     await window.ethereum.enable();
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -1131,37 +1059,48 @@ async function get_account() {
        });
 }
 
+// Get the wallet address from the contract
 async function get_wallet_address() {
     FarmlandCrowdsaleToken.methods.wallet().call().then(wallet_address_populate);
 }
+
+// Get the wallet address from the contract and do not update any UI field
 async function get_wallet_address_non_ui() {
     FarmlandCrowdsaleToken.methods.wallet().call();
 }
+
+// Get the account balance related to the purchaser's account - gives the tokens that the purchaser's owns
 async function get_account_balance(purchasers_account) {
     FarmlandToken.methods.balanceOf(purchasers_account).call().then(account_balance_populate);
 }
 
+// Get the purchaser's account from the UI's currentAccount field and get the token balance 
 async function get_current_tokens() {
     var purchasers_account = document.getElementById("currentAccount").getAttribute("value")
     FarmlandToken.methods.balanceOf(purchasers_account).call().then(current_tokens_populate);
 }
 
+// Get wei estimate from the contract related to the tokens desired
 async function get_wei_estimate(tokens_desired) {
     console.log("get_wei_estimate");
     console.log("token_desired" + tokens_desired)
     FarmlandCrowdsaleToken.methods.getWeiEstimate(tokens_desired).call().then(wei_estimate_populate);
     console.log("get_wei_estimate done");
 }
+
+// Get the current token rate from the contract 
 async function get_rate() {
     FarmlandCrowdsaleToken.methods.rate().call().then(rate_populate);
 }
 
+// Process the estimate wei button and get the token desired from the UI and retrieve the wei estimate from the contract
 async function estimate_wei_required() {
     var tokensDesired = document.getElementById("tokensDesired").value.toString();
     console.log("estimate_wei_required " + tokensDesired)
     get_wei_estimate(tokensDesired);
 }
 
+// Process the purchase tokens button. Use the wallet address, current account and wei estimate fields from the UI
 async function purchase_tokens() {
     var crowdsale_contract_address = document.getElementById('walletAddress').value;
     var purchasers_account = document.getElementById('currentAccount').value;
@@ -1170,6 +1109,7 @@ async function purchase_tokens() {
     purchase_tokens_finalize(purchasers_account, crowdsale_contract_address, wei_estimate);
 }
 
+// USe the contract's buy tokens to purchase the appropriate number of tokens
 async function purchase_tokens_finalize(purchasers_account, crowdsale_contract_address, wei_estimate) {
     console.log("purchase_tokens_finalize");
     var tx_hash = await FarmlandCrowdsaleToken.methods.buyTokens(purchasers_account).send({
@@ -1179,15 +1119,17 @@ async function purchase_tokens_finalize(purchasers_account, crowdsale_contract_a
             "gasPrice":0,
             "value":wei_estimate
         });
-    //window.web3.eth.wait_for_transaction_receipt(tx_hash);
 }
 
+// Update the weiEstimate UI field with the wei estimate value
 function wei_estimate_populate(_value) {
     console.log("Wei Estimate ");
     console.log("Wei Estimate : " + _value);
     document.getElementById("weiEstimate").removeAttribute("disabled");
     document.getElementById("weiEstimate").setAttribute("value", _value);
 }
+
+// Update the currentTokens UI field with the current token owned by the purchaser
 function current_tokens_populate(_value) {
     var balance = _value;
 
@@ -1195,11 +1137,13 @@ function current_tokens_populate(_value) {
     document.getElementById("currentTokens").setAttribute("value", "Current Tokens Owned : " + _value);
 }
 
+// Update the wallet address in the UI
 function wallet_address_populate(_value) {
     document.getElementById("walletAddress").removeAttribute("disabled");
     document.getElementById("walletAddress").setAttribute("value", _value);
 }
 
+// Update the wallet wei balance in the UI
 async function wallet_balance() {
     console.log(document.getElementById('walletAddress').value);
     console.log(await window.web3.eth.getBalance(document.getElementById('walletAddress').value));
@@ -1209,45 +1153,63 @@ async function wallet_balance() {
     var balance = window.web3.utils.fromWei(balanceToString, 'ether');
     document.getElementById("walletBalance").setAttribute("value", "Wallet Balance : " + balance);
 }
+
+// Update the purchaser's account's balance in the UI
 async function account_balance() {
     var balanceToString = await window.web3.eth.getBalance(document.getElementById('currentAccount').value.toString());
     var balance = window.web3.utils.fromWei(balanceToString, 'ether');
     document.getElementById("accountBalance").setAttribute("value", "Account Balance : " + balance);
 }
 
+// Update the parcel details in the UI
 function parcel_details_populate(_value) {
     document.getElementById("parcelDetails").removeAttribute("disabled");
     document.getElementById("parcelDetails").setAttribute("value", "Parcel Details : " + _value);
 }
+
+// Update the parcel owner's name in the UI
 function parcel_owner_populate(_value) {
     document.getElementById("parcelOwner").removeAttribute("disabled");
     document.getElementById("parcelOwner").setAttribute("value", "Parcel Owner : " + _value);
 }
+
+// Update the parcel acres in the UI
 function parcel_acres_populate(_value) {
     document.getElementById("parcelAcres").removeAttribute("disabled");
     document.getElementById("parcelAcres").setAttribute("value", "Parcel Acres : " + _value);
 }
+
+// Update the parcel contract name in the UI
 function parcel_name_populate(_value) {
     document.getElementById("parcelName").removeAttribute("disabled");
     document.getElementById("parcelName").setAttribute("value", "Parcel Name : " + _value);
 }
+
+// Update the parcel contract symbol in the UI
 function parcel_symbol_populate(_value) {
     document.getElementById("parcelSymbol").removeAttribute("disabled");
     document.getElementById("parcelSymbol").setAttribute("value", "Parcel Symbol : " + _value);
 }
+
+// Update the max tokens limit in the UI
 function parcel_cap_populate(_value) {
     document.getElementById("parcelCap").removeAttribute("disabled");
     document.getElementById("parcelCap").setAttribute("value", "Parcel Total Tokens : " + _value);
 }
+
+// Update the tokens issued thus far in the UI
 function parcel_tokens_issued_populate(_value) {
     document.getElementById("parcelTokensIssued").removeAttribute("disabled");
     document.getElementById("parcelTokensIssued").setAttribute("value", "Parcel Tokens Issued : " + _value);
 }
+
+// Update the Wei raised so far in the ui
 function parcel_wei_raised_populate(_value) {
     document.getElementById("parcelWeiRaised").removeAttribute("disabled");
     document.getElementById("parcelWeiRaised").setAttribute("value", "Parcel Wei Raised : " + _value);
 }
 
+// Get the wallet balance, account balance and current tokens 
 async function get_balances() {
     console.log(document.getElementById('currentAccount').value);
     wallet_balance();
@@ -1255,11 +1217,8 @@ async function get_balances() {
     get_current_tokens();
 }
 
+// Process the connect to metamask button and retrieve the purchaser's account
 async function login() {
-    // const loggers = await window.ethereum.getAccount(console.log);
-    // await window.ethereum.enable();
-    // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
-
     const walletAddress = await window.ethereum.request({
         method: "eth_requestAccounts",
         params: [
@@ -1269,43 +1228,30 @@ async function login() {
         ]
       });
       
-      //if (!isReturningUser) {
-      // Runs only they are brand new, or have hit the disconnect button
-        await window.ethereum.request({
-          method: "wallet_requestPermissions",
-          params: [
+    
+    await window.ethereum.request({
+        method: "wallet_requestPermissions",
+        params: [
             {
               eth_accounts: {}
             }
-          ]
-        });
-      //}
-
+        ]
+    });
+    
+	// Enabled the various fields
     document.getElementById("balance").removeAttribute("disabled");
     document.getElementById("walletAddress").removeAttribute("disabled");
     document.getElementById("walletBalance").removeAttribute("disabled");
     document.getElementById("currentAccount").removeAttribute("disabled");
     document.getElementById("accountBalance").removeAttribute("disabled");
-    get_current_account();
+    
+	// Get the current account and wallet address
+	get_current_account();
     get_wallet_address();
     
-
+	// Enable some more fields
     document.getElementById("tokensDesired").removeAttribute("disabled");
     document.getElementById("weiEstimate").removeAttribute("disabled");
     document.getElementById("estimateWei").removeAttribute("disabled");
     document.getElementById("purchaseTokens").removeAttribute("disabled");
-    
-    //Moralis.Web3.authenticate().then(function (user) {
-    //    user.set("name", document.getElementById('username').value);
-    //    user.set("name", document.getElementById('username').value);
-    //    user.save();
-//        document.getElementById("gameid").removeAttribute("disabled");
-        
-        
-        // wallet_balance();
-        // account_balance(current_account);
-        // get_current_tokens();
-    // })
-    
-    
 }
